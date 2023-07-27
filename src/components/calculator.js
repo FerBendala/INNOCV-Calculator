26e7195015adfb3d720556f3657e109bd3341c2e
fernando.bendala <f.bendala@shopfully.com>
2023-07-27 17:49:02 +0200

    Improve logic of calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -4,9 +4,11 @@ import Keypad from './keypad'
 
 const Calculator = () => {
     const [displayValue, setDisplayValue] = useState( '0' )
+    const [fullExpression, setFullExpression] = useState( '' )
     const [firstOperand, setFirstOperand] = useState( null )
     const [operator, setOperator] = useState( null )
     const [waitingForSecondOperand, setWaitingForSecondOperand] = useState( false )
+    const [hasError, setHasError] = useState( false )
 
     const handleNumberClick = ( number ) => {
         if ( waitingForSecondOperand ) {
@@ -15,14 +17,25 @@ const Calculator = () => {
         } else {
             setDisplayValue( displayValue === '0' ? String( number ) : displayValue + number )
         }
+        setFullExpression( fullExpression + number )
     }
 
     const handleOperatorClick = ( nextOperator ) => {
         const inputValue = parseFloat( displayValue )
 
+        if ( !inputValue && fullExpression === '' ) {
+            // Do nothing if the expression is empty or starts with an operator
+            return
+        }
+
         if ( firstOperand === null ) {
             setFirstOperand( inputValue )
         } else if ( operator ) {
+            // Additional checks to prevent consecutive operators
+            if ( !waitingForSecondOperand ) {
+                const newExpression = fullExpression.slice( 0, -1 ) + nextOperator
+                setFullExpression( newExpression )
+            }
             const result = performOperation( firstOperand, inputValue, operator )
             setDisplayValue( String( result ) )
             setFirstOperand( result )
@@ -30,15 +43,22 @@ const Calculator = () => {
 
         setWaitingForSecondOperand( true )
         setOperator( nextOperator )
+        setFullExpression( fullExpression + nextOperator )
     }
 
     const handleEqualClick = () => {
         const inputValue = parseFloat( displayValue )
 
         if ( operator && firstOperand !== null ) {
-            const result = performOperation( firstOperand, inputValue, operator )
-            setDisplayValue( String( result ) )
-            setFirstOperand( result )
+            if ( operator === '/' && inputValue === 0 ) {
+                setDisplayValue( 'Error: Division by zero' )
+                setHasError( true )
+            } else {
+                const result = performOperation( firstOperand, inputValue, operator )
+                setDisplayValue( String( result ) )
+                setFirstOperand( result )
+                setFullExpression( result )
+            }
             setOperator( null )
             setWaitingForSecondOperand( false )
         }
@@ -46,9 +66,11 @@ const Calculator = () => {
 
     const handleClearClick = () => {
         setDisplayValue( '0' )
+        setFullExpression( '' )
         setFirstOperand( null )
         setOperator( null )
         setWaitingForSecondOperand( false )
+        setHasError( false )
     }
 
     const performOperation = ( operand1, operand2, operator ) => {
@@ -68,7 +90,11 @@ const Calculator = () => {
 
     return (
         <div className="calculator">
-            <Display value={displayValue} />
+            <Display
+                value={displayValue}
+                fullExpression={fullExpression}
+                hasError={hasError}
+            />
             <Keypad
                 onNumberClick={handleNumberClick}
                 onOperatorClick={handleOperatorClick}
--- src/components/display.js
+++ src/components/display.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const Display = ( { value } ) => {
-    return <div className="display">{value}</div>
+const Display = ( { value, fullExpression, hasError } ) => {
+    const displayClassName = hasError ? 'display error' : 'display'
+
+    return (
+        <div className={displayClassName}>
+            <div className="current-value">{value}</div>
+            <div className="full-expression">{fullExpression || 0}</div>
+        </div>
+    )
 }
 
 export default Display
